646f3a3fbbfc873dff4bc996e5f0392366671136
Hankscr459 <defcw58@gmail.com>
2026-01-30 11:04:12 +0800

    test: 測試補Console

--- MyErpManagement.Api/Controllers/AuthController.cs
+++ MyErpManagement.Api/Controllers/AuthController.cs
@@ -104,6 +104,9 @@ namespace MyErpManagement.Api.Controllers
                 .GetFirstOrDefaultAsync(u => u.Email == verifyEmailRequestDto.Email);
             if (existingUser is not null)
             {
+                Console.WriteLine("Email already in use: " + existingUser.Email);
+                Console.WriteLine("Email already in use: " + existingUser.Account);
+                Console.WriteLine("Email already in use: " + existingUser.Id);
                 return Conflict(new ApiResponseDto(HttpStatusCode.Conflict, ResponseTextConstant.Conflict.EmailAlreadyInUse));
             }
             var emailCode = await cachService.GetRegistCodeAsync(verifyEmailRequestDto.Email);
