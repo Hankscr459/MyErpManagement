74686d0997cfdfa3aae08899f9472f1063ae4098
Hankscr459 <defcw58@gmail.com>
2026-02-13 21:23:34 +0800

    feat: 新增庫存 AverageCost欄位  #5

---
+++ MyErpManagement.DataBase/Migrations/20260213131617_UpdateInventoryAverageCostField.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyErpManagement.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateInventoryAverageCostField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "AverageCost",
+                table: "Inventories",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AverageCost",
+                table: "Inventories");
+        }
+    }
+}
