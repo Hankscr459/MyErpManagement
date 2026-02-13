4608d01ef8d721ee8afbe3fbb754c9e96a4fca3e
Hankscr459 <defcw58@gmail.com>
2026-02-13 21:24:47 +0800

    feat: 採購單 新增 status 負責人 欄位  #5

---
+++ MyErpManagement.DataBase/Migrations/20260213132035_UpdatePurchaseOrderStatusByField.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyErpManagement.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatePurchaseOrderStatusByField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "ApprovedBy",
+                table: "PurchaseOrders",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CancelBy",
+                table: "PurchaseOrders",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "CompleteBy",
+                table: "PurchaseOrders",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ApprovedBy",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "CancelBy",
+                table: "PurchaseOrders");
+
+            migrationBuilder.DropColumn(
+                name: "CompleteBy",
+                table: "PurchaseOrders");
+        }
+    }
+}
