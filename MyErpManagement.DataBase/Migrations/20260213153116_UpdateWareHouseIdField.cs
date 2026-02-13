5b3b5fa3c07b77ada5af000747485604f195b2b2
Hankscr459 <defcw58@gmail.com>
2026-02-13 23:33:47 +0800

    fix: typo Db table Field

---
+++ MyErpManagement.DataBase/Migrations/20260213153116_UpdateWareHouseIdField.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MyErpManagement.DataBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateWareHouseIdField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PurchaseOrders_WareHouses_WarehouseId",
+                table: "PurchaseOrders");
+
+            migrationBuilder.RenameColumn(
+                name: "WarehouseId",
+                table: "PurchaseOrders",
+                newName: "WareHouseId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PurchaseOrders_WarehouseId",
+                table: "PurchaseOrders",
+                newName: "IX_PurchaseOrders_WareHouseId");
+
+            migrationBuilder.RenameColumn(
+                name: "WarehouseId",
+                table: "InventoryTransactions",
+                newName: "WareHouseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PurchaseOrders_WareHouses_WareHouseId",
+                table: "PurchaseOrders",
+                column: "WareHouseId",
+                principalTable: "WareHouses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PurchaseOrders_WareHouses_WareHouseId",
+                table: "PurchaseOrders");
+
+            migrationBuilder.RenameColumn(
+                name: "WareHouseId",
+                table: "PurchaseOrders",
+                newName: "WarehouseId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_PurchaseOrders_WareHouseId",
+                table: "PurchaseOrders",
+                newName: "IX_PurchaseOrders_WarehouseId");
+
+            migrationBuilder.RenameColumn(
+                name: "WareHouseId",
+                table: "InventoryTransactions",
+                newName: "WarehouseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PurchaseOrders_WareHouses_WarehouseId",
+                table: "PurchaseOrders",
+                column: "WarehouseId",
+                principalTable: "WareHouses",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
